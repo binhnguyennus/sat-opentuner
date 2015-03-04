61ab6f320975d8be46ca73081ccda17e85f9536f
Pedro Bruel <pedro.bruel@gmail.com>
2015-03-04 20:42:27 -0300

    Preparing portfolio.

--- portfolio/cg_multi.py
+++ portfolio/cg_multi.py
@@ -3,6 +3,7 @@ import time
 from subprocess import call
 from multiprocessing import Pool, cpu_count
 
+EXEC_DIR = 'sat-opentuner'
 SOLVERS_DIR = 'solvers/'
 INSTANCES_DIR = 'instances/sat_lib/'
 INSTANCES = 'instance_set_3.txt'
@@ -17,19 +18,13 @@ def ccanr_glucose(filename):
     call(cmd + instance + args, stdout=open(os.devnull, 'wb'), shell=True)
     return
 
-def cb(one):
-    print 'oi\n'
-
-with open(INSTANCES, 'r') as instance_file:    
-    lines = instance_file.read().splitlines()
-
-#complete_lines = lines
-#lines[:] = [INSTANCES_DIR + l for l in lines]
-
 if __name__ == '__main__':
+    PROJECT_DIR = os.getcwd().split(EXEC_DIR)[0]
+    os.chdir(PROJECT_DIR + EXEC_DIR)
+    with open(INSTANCES, 'r') as instance_file:    
+        lines = instance_file.read().splitlines()
+
     pool = Pool(cpu_count())
-    #result = pool.map_async(ccanr_glucose, lines)
-    #result.get()
     for line in lines:
         pool.apply_async(ccanr_glucose, args=(INSTANCES_DIR + line, ))
 
