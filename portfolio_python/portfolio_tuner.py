85dbc11b873e8c135bdee37f1c11d26772676342
Pedro Bruel <pedro.bruel@gmail.com>
2015-03-14 12:33:34 -0300

    Implemented a working tuner prototype.

--- portfolio_python/portfolio_tuner.py
+++ portfolio_python/portfolio_tuner.py
@@ -10,17 +10,38 @@ from opentuner import ConfigurationManipulator, EnumParameter, MeasurementInterf
 class PortfolioTuner(MeasurementInterface):
     def manipulator(self):
         manipulator = ConfigurationManipulator()
-        values = [1, 2, 5, 8, 10]
+        values = [0, 1, 2, 5, 8, 10]
         name = 'solver_'
         for i in range (9):
             manipulator.add_parameter(EnumParameter(name + str(i), values))
         return manipulator
 
     def run(self, desired_result, input, limit):
-        return Result()
+        cfg = desired_result.configuration.data
+        cmd = CMD
+        name = 'solver_'
+        for i in range(9):
+            cmd += ' ' + str(cfg[name + str(i)])
+
+        run_result = self.call_program(cmd)
+        return Result(time=run_result['time'])
+
+    def save_final_config(self, configuration):
+        cfg = configuration.data
+        cmd = CMD
+        name = 'solver_'
+
+        for i in range(9):
+            cmd += ' ' + str(cfg[name + str(i)])
+
+        print 'Optimal config written to optimal.txt: ', cmd
+        with open('optimal.txt', 'a+') as myfile:
+            myfile.write("/usr/bin/time -p " + cmd + "\n")
 
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser(parents=opentuner.argparsers())
     args = argparser.parse_args()
+
+    CMD = 'python sat_solver.py -rs'
 
     PortfolioTuner.main(args)
