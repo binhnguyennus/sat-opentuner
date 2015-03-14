d074de86991ba422e69fe87901261cc8d441542e
Pedro Bruel <pedro.bruel@gmail.com>
2015-03-14 15:16:04 -0300

    Added configuration-validation prototype.

--- portfolio_python/portfolio_tuner.py
+++ portfolio_python/portfolio_tuner.py
@@ -7,24 +7,38 @@ import opentuner
 import argparse
 from opentuner import ConfigurationManipulator, EnumParameter, MeasurementInterface, Result
 
+class PortfolioManipulator(ConfigurationManipulator):
+    def __init__(self):
+        super(PortfolioManipulator, self).__init__()
+
+    def validate(self, config):
+        values = config.data.values()
+        return not (sum(values) > 10 or all([v == 0 for v in values]))
+
 class PortfolioTuner(MeasurementInterface):
     def manipulator(self):
-        manipulator = ConfigurationManipulator()
+        self.manipulator = PortfolioManipulator()
         values = [0, 1, 2, 5, 8, 10]
         name = 'solver_'
         for i in range (9):
-            manipulator.add_parameter(EnumParameter(name + str(i), values))
-        return manipulator
+            self.manipulator.add_parameter(EnumParameter(name+str(i), values))
+        return self.manipulator
 
     def run(self, desired_result, input, limit):
         cfg = desired_result.configuration.data
         cmd = CMD
         name = 'solver_'
+
         for i in range(9):
             cmd += ' ' + str(cfg[name + str(i)])
 
-        run_result = self.call_program(cmd)
-        return Result(time=run_result['time'])
+        if (self.manipulator.validate(desired_result.configuration)):
+            run_result = self.call_program(cmd)
+            result = run_result['time']
+        else:
+            result = 99999999 
+
+        return Result(time = result)
 
     def save_final_config(self, configuration):
         cfg = configuration.data
